3072d27a88e68d0a929d44d7c016cf48a7f6be7d
Gammaglobulino <gammaglobulino@ubuntu.ubuntu-domain>
2011-12-27 10:26:30 +0100

    primo commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoMazzaApp::Application.config.secret_token = '7f7b8054bc30148edbbf0e9fe37f2974d74c6dd0716ea491d3cf713430c82762d567bdb1ecfab2904442830054e8ac1790c5dab47ee9b13fdec0e1101b4c2c08'
